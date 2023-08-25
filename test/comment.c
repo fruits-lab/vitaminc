08a759c04877a25df392568e8e70925cee19d042
Lai-YT <381xvmvbib@gmail.com>
2023-08-25 16:25:43 +0800

    Add test on comments

---
+++ test/comment.c
@@ -0,0 +1,16 @@
+// C++ style comment
+/* C style comment */
+
+int main(/* C style inline comment */) {  // C++ style inline comment
+
+  // C style in C++ style: /*
+  // */
+
+  /* C++ style in C++ style: // */
+
+  /* Multiple lines;
+   * nested C style comment is not supported by C99
+   */
+
+  return 0;
+}
