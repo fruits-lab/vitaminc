7dc5d690c4fb74c166d3092f6a89dcabcd2426a5
Lai-YT <381xvmvbib@gmail.com>
2023-09-15 13:59:54 +0800

    Implement dump of modulo expression node

--- src/ast.cpp
+++ src/ast.cpp
@@ -282,7 +282,9 @@ std::string DivExprNode::Op_() const {
 
 std::string ModExprNode::OpName_() const {}
 
-std::string ModExprNode::Op_() const {}
+std::string ModExprNode::Op_() const {
+  return "%";
+}
 
 std::string GreaterThanExprNode::OpName_() const {
   // signed
