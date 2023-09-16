2f763afc2dbc4e97054d33bcb52d600c7a9f7dd1
Lai-YT <381xvmvbib@gmail.com>
2023-09-16 17:51:48 +0800

    Introduce the type checking visitor

---
+++ include/type_checker.hpp
@@ -0,0 +1,27 @@
+#ifndef TYPE_CHECKER_HPP_
+#define TYPE_CHECKER_HPP_
+
+#include "scope.hpp"
+#include "visitor.hpp"
+
+/// @brief A modifying pass; resolves the type of expressions.
+class TypeChecker : public Visitor<true> {
+ public:
+  TypeChecker(ScopeStack& env) : env_{env} {}
+
+  void Visit(DeclNode&) override;
+  void Visit(BlockStmtNode&) override;
+  void Visit(ProgramNode&) override;
+  void Visit(NullStmtNode&) override;
+  void Visit(ReturnStmtNode&) override;
+  void Visit(ExprStmtNode&) override;
+  void Visit(IdExprNode&) override;
+  void Visit(IntConstExprNode&) override;
+  void Visit(BinaryExprNode&) override;
+  void Visit(SimpleAssignmentExprNode&) override;
+
+ private:
+  ScopeStack& env_;
+};
+
+#endif  // TYPE_CHECKER_HPP_
