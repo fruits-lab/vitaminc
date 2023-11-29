f8bc184e65d3a47e42c63b1b34e88112bcba1e1a
Lee <lee10202013@gmail.com>
2023-11-30 00:11:51 +0800

    Add while statement typecheck test cases

---
+++ test/typecheck/do_while_stmt.c
@@ -0,0 +1,8 @@
+int main() {
+  int i = 0;
+  do {
+    i = i + 1;
+  } while (i < 5);
+
+  return 0;
+}
---
+++ test/typecheck/while_stmt.c
@@ -0,0 +1,8 @@
+int main() {
+  int i = 5;
+  while (i > 0) {
+    i = i - 1;
+  }
+
+  return 0;
+}
