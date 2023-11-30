063afd263bdf0c714002a599f82297baece215a1
Lee <lee10202013@gmail.com>
2023-11-30 15:46:56 +0800

    Move block statement to statement

--- include/ast.hpp
+++ include/ast.hpp
@@ -94,9 +94,8 @@ struct NullStmtNode : public StmtNode {
 };
 
 struct IfStmtNode : public StmtNode {
-  IfStmtNode(std::unique_ptr<ExprNode> expr,
-             std::unique_ptr<BlockStmtNode> then,
-             std::unique_ptr<BlockStmtNode> or_else = {})
+  IfStmtNode(std::unique_ptr<ExprNode> expr, std::unique_ptr<StmtNode> then,
+             std::unique_ptr<StmtNode> or_else = {})
       : predicate{std::move(expr)},
         then{std::move(then)},
         or_else{std::move(or_else)} {}
@@ -105,8 +104,8 @@ struct IfStmtNode : public StmtNode {
   virtual void Accept(ModifyingVisitor&) override;
 
   std::unique_ptr<ExprNode> predicate;
-  std::unique_ptr<BlockStmtNode> then;
-  std::unique_ptr<BlockStmtNode> or_else;
+  std::unique_ptr<StmtNode> then;
+  std::unique_ptr<StmtNode> or_else;
 };
 
 struct ReturnStmtNode : public StmtNode {
