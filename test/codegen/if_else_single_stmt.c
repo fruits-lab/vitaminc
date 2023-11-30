a09d843dfd634d04fbf4c00ff98d164e9161f2df
Lee <lee10202013@gmail.com>
2023-11-30 15:44:47 +0800

    Add if statement test cases for single statement body

---
+++ test/codegen/if_else_single_stmt.c
@@ -0,0 +1,9 @@
+int main() {
+  int i = 2;
+  if (i > 0)
+    i = i - 1;
+  else
+    i = i + 1;
+
+  return i;
+}
---
+++ test/typecheck/if_single_stmt.c
@@ -0,0 +1,7 @@
+int main() {
+  int i = 2;
+  if (i > 0)
+    i = i + 1;
+
+  return 0;
+}
