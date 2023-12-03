8f39d07bb9c28ea724dafd2ead3ea8e255f306e1
Lee <lee10202013@gmail.com>
2023-12-03 14:31:03 +0800

    Implement for statement ast_dumper

--- include/qbe_ir_generator.hpp
+++ include/qbe_ir_generator.hpp
@@ -5,14 +5,17 @@
 
 class QbeIrGenerator : public NonModifyingVisitor {
  public:
+  void Visit(const LoopInitNode&) override;
   void Visit(const DeclNode&) override;
   void Visit(const BlockStmtNode&) override;
   void Visit(const ProgramNode&) override;
   void Visit(const NullStmtNode&) override;
   void Visit(const IfStmtNode&) override;
   void Visit(const WhileStmtNode&) override;
+  void Visit(const ForStmtNode&) override;
   void Visit(const ReturnStmtNode&) override;
   void Visit(const ExprStmtNode&) override;
+  void Visit(const NullExprNode&) override;
   void Visit(const IdExprNode&) override;
   void Visit(const IntConstExprNode&) override;
   void Visit(const BinaryExprNode&) override;
