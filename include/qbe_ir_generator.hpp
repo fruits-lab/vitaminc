f9cbb89749d5736ada5cfa6fe51cdc3d6b72a755
Lai-YT <381xvmvbib@gmail.com>
2024-02-24 15:49:47 +0800

    Pass by reference instead of sharing as global
    Bison allows the user to define parameters of the parser interface,
    so that the root of our AST can be passed by reference and built by the parser.
    For more details on the parser interface, refer to
    https://www.gnu.org/software/bison/manual/html_node/C_002b_002b-Parser-Interface.html.

--- include/qbe_ir_generator.hpp
+++ include/qbe_ir_generator.hpp
@@ -1,6 +1,11 @@
 #ifndef QBE_IR_GENERATOR_HPP_
 #define QBE_IR_GENERATOR_HPP_
 
+#include <fmt/core.h>
+
+#include <iosfwd>
+
+#include "ast.hpp"
 #include "visitor.hpp"
 
 class QbeIrGenerator : public NonModifyingVisitor {
@@ -22,6 +27,25 @@ class QbeIrGenerator : public NonModifyingVisitor {
   void Visit(const UnaryExprNode&) override;
   void Visit(const BinaryExprNode&) override;
   void Visit(const SimpleAssignmentExprNode&) override;
+
+  QbeIrGenerator(std::ostream& output) : output_{output} {}
+
+ private:
+  std::ostream& output_;
+
+  /// @brief Writes out the formatted string to `output`.
+  /// @note This is a convenience function to avoid having to pass `output`
+  /// everywhere.
+  template <typename... T>
+  void WriteOut_(
+      fmt::format_string<T...> format,
+      T&&... args) {  // NOLINT(cppcoreguidelines-missing-std-forward):
+                      // `make_format_args` takes rvalue references.
+    VWriteOut_(format, fmt::make_format_args(args...));
+  }
+
+  /// @note This function is not meant to be used directly.
+  void VWriteOut_(fmt::string_view format, fmt::format_args args);
 };
 
 #endif  // QBE_IR_GENERATOR_HPP_
