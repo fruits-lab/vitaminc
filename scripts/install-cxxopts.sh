82491d97f39d4afffdf4540f0bcfd48dd5926bf4
Lai-YT <381xvmvbib@gmail.com>
2024-02-25 22:11:46 +0800

    Drop build of cxxopts examples and tests

--- scripts/install-cxxopts.sh
+++ scripts/install-cxxopts.sh
@@ -3,7 +3,7 @@
 wget https://github.com/jarro2783/cxxopts/archive/refs/tags/v3.1.1.tar.gz -O - | tar zxf - &&
   cd cxxopts-3.1.1/ &&
   mkdir build/ &&
-  cmake -B build/ . &&
+  cmake -DCXXOPTS_BUILD_EXAMPLES=OFF -DCXXOPTS_BUILD_TESTS=OFF -B build/ . &&
   sudo make -C build/ install &&
   cd .. &&
   rm -rf cxxopts-3.1.1/
