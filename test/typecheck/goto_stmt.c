abb7222cef4f7f39c1701c3b21207e9d4a85bffa
Lai-YT <381xvmvbib@gmail.com>
2024-04-02 21:18:47 +0800

    Test visibility of label across outer block scope
    A label, being of function scope, should remain visible across all block
    scopes within the same function.
    Additionally, we are not testing identifiers of other scope kinds as
    they consistently adhere to the nesting rule.

--- test/typecheck/goto_stmt.c
+++ test/typecheck/goto_stmt.c
@@ -1,7 +1,11 @@
 int main() {
   int i = 0;
-begin:
-  i = i + 1;
+  {
+  begin:
+    i = i + 1;
+  }
+  // Since label is of function scope,
+  // it should still be visible here.
   goto begin;
   return 0;
 }
