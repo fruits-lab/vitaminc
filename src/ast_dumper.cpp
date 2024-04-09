f94edd88e18241d89d9c601ad4821b8eca780883
Lai-YT <381xvmvbib@gmail.com>
2024-04-09 23:44:48 +0800

    Dump location of AST nodes

--- src/ast_dumper.cpp
+++ src/ast_dumper.cpp
@@ -66,8 +66,8 @@ std::string GetUnaryOperator(UnaryOperator op) {
 }  // namespace
 
 void AstDumper::Visit(const DeclNode& decl) {
-  std::cout << indenter_.Indent() << "DeclNode " << decl.id << ": "
-            << TypeToString(decl.type) << '\n';
+  std::cout << indenter_.Indent() << "DeclNode <" << decl.loc << "> " << decl.id
+            << ": " << TypeToString(decl.type) << '\n';
 
   if (decl.init) {
     indenter_.IncreaseLevel();
@@ -77,13 +77,13 @@ void AstDumper::Visit(const DeclNode& decl) {
 }
 
 void AstDumper::Visit(const ParamNode& parameter) {
-  std::cout << indenter_.Indent() << "ParamNode " << parameter.id << ": "
-            << TypeToString(parameter.type) << '\n';
+  std::cout << indenter_.Indent() << "ParamNode <" << parameter.loc << "> "
+            << parameter.id << ": " << TypeToString(parameter.type) << '\n';
 }
 
 void AstDumper::Visit(const FuncDefNode& func_def) {
-  std::cout << indenter_.Indent() << "FuncDefNode " << func_def.id << ": "
-            << TypeToString(func_def.type) << '\n';
+  std::cout << indenter_.Indent() << "FuncDefNode <" << func_def.loc << "> "
+            << func_def.id << ": " << TypeToString(func_def.type) << '\n';
 
   indenter_.IncreaseLevel();
   for (const auto& parameter : func_def.parameters) {
@@ -94,7 +94,7 @@ void AstDumper::Visit(const FuncDefNode& func_def) {
 }
 
 void AstDumper::Visit(const LoopInitNode& loop_init) {
-  std::cout << indenter_.Indent() << "LoopInitNode\n";
+  std::cout << indenter_.Indent() << "LoopInitNode <" << loop_init.loc << ">\n";
   indenter_.IncreaseLevel();
   std::visit([this](auto&& clause) { clause->Accept(*this); },
              loop_init.clause);
@@ -102,7 +102,8 @@ void AstDumper::Visit(const LoopInitNode& loop_init) {
 }
 
 void AstDumper::Visit(const CompoundStmtNode& compound_stmt) {
-  std::cout << indenter_.Indent() << "CompoundStmtNode\n";
+  std::cout << indenter_.Indent() << "CompoundStmtNode <" << compound_stmt.loc
+            << ">\n";
   indenter_.IncreaseLevel();
   for (const auto& item : compound_stmt.items) {
     std::visit([this](auto&& item) { item->Accept(*this); }, item);
@@ -111,7 +112,7 @@ void AstDumper::Visit(const CompoundStmtNode& compound_stmt) {
 }
 
 void AstDumper::Visit(const ProgramNode& program) {
-  std::cout << indenter_.Indent() << "ProgramNode\n";
+  std::cout << indenter_.Indent() << "ProgramNode <" << program.loc << ">\n";
   indenter_.IncreaseLevel();
   for (const auto& func_def : program.func_def_list) {
     func_def->Accept(*this);
@@ -120,7 +121,7 @@ void AstDumper::Visit(const ProgramNode& program) {
 }
 
 void AstDumper::Visit(const IfStmtNode& if_stmt) {
-  std::cout << indenter_.Indent() << "IfStmtNode\n";
+  std::cout << indenter_.Indent() << "IfStmtNode <" << if_stmt.loc << ">\n";
   indenter_.IncreaseLevel();
   if_stmt.predicate->Accept(*this);
   std::cout << indenter_.Indent() << "// Then\n";
@@ -135,7 +136,8 @@ void AstDumper::Visit(const IfStmtNode& if_stmt) {
 }
 
 void AstDumper::Visit(const WhileStmtNode& while_stmt) {
-  std::cout << indenter_.Indent() << "WhileStmtNode\n";
+  std::cout << indenter_.Indent() << "WhileStmtNode <" << while_stmt.loc
+            << ">\n";
   if (while_stmt.is_do_while) {
     indenter_.IncreaseLevel();
     std::cout << indenter_.Indent() << "// Do\n";
@@ -153,7 +155,7 @@ void AstDumper::Visit(const WhileStmtNode& while_stmt) {
 }
 
 void AstDumper::Visit(const ForStmtNode& for_stmt) {
-  std::cout << indenter_.Indent() << "ForStmtNode\n";
+  std::cout << indenter_.Indent() << "ForStmtNode <" << for_stmt.loc << ">\n";
   indenter_.IncreaseLevel();
   for_stmt.loop_init->Accept(*this);
   for_stmt.predicate->Accept(*this);
@@ -163,26 +165,31 @@ void AstDumper::Visit(const ForStmtNode& for_stmt) {
 }
 
 void AstDumper::Visit(const ReturnStmtNode& ret_stmt) {
-  std::cout << indenter_.Indent() << "ReturnStmtNode\n";
+  std::cout << indenter_.Indent() << "ReturnStmtNode <" << ret_stmt.loc
+            << ">\n";
   indenter_.IncreaseLevel();
   ret_stmt.expr->Accept(*this);
   indenter_.DecreaseLevel();
 }
 
 void AstDumper::Visit(const GotoStmtNode& goto_stmt) {
-  std::cout << indenter_.Indent() << "GotoStmtNode " << goto_stmt.label << '\n';
+  std::cout << indenter_.Indent() << "GotoStmtNode <" << goto_stmt.loc << "> "
+            << goto_stmt.label << '\n';
 }
 
 void AstDumper::Visit(const BreakStmtNode& break_stmt) {
-  std::cout << indenter_.Indent() << "BreakStmtNode\n";
+  std::cout << indenter_.Indent() << "BreakStmtNode <" << break_stmt.loc
+            << ">\n";
 }
 
 void AstDumper::Visit(const ContinueStmtNode& continue_stmt) {
-  std::cout << indenter_.Indent() << "ContinueStmtNode\n";
+  std::cout << indenter_.Indent() << "ContinueStmtNode <" << continue_stmt.loc
+            << ">\n";
 }
 
 void AstDumper::Visit(const SwitchStmtNode& switch_stmt) {
-  std::cout << indenter_.Indent() << "SwitchStmtNode\n";
+  std::cout << indenter_.Indent() << "SwitchStmtNode <" << switch_stmt.loc
+            << ">\n";
   indenter_.IncreaseLevel();
   switch_stmt.ctrl->Accept(*this);
   switch_stmt.stmt->Accept(*this);
@@ -190,15 +197,15 @@ void AstDumper::Visit(const SwitchStmtNode& switch_stmt) {
 }
 
 void AstDumper::Visit(const IdLabeledStmtNode& id_labeled_stmt) {
-  std::cout << indenter_.Indent() << "IdLabeledStmtNode "
-            << id_labeled_stmt.label << '\n';
+  std::cout << indenter_.Indent() << "IdLabeledStmtNode <"
+            << id_labeled_stmt.loc << "> " << id_labeled_stmt.label << '\n';
   indenter_.IncreaseLevel();
   id_labeled_stmt.stmt->Accept(*this);
   indenter_.DecreaseLevel();
 }
 
 void AstDumper::Visit(const CaseStmtNode& case_stmt) {
-  std::cout << indenter_.Indent() << "CaseStmtNode\n";
+  std::cout << indenter_.Indent() << "CaseStmtNode <" << case_stmt.loc << ">\n";
   indenter_.IncreaseLevel();
   case_stmt.expr->Accept(*this);
   case_stmt.stmt->Accept(*this);
@@ -206,35 +213,37 @@ void AstDumper::Visit(const CaseStmtNode& case_stmt) {
 }
 
 void AstDumper::Visit(const DefaultStmtNode& default_stmt) {
-  std::cout << indenter_.Indent() << "DefaultStmtNode\n";
+  std::cout << indenter_.Indent() << "DefaultStmtNode <" << default_stmt.loc
+            << ">\n";
   indenter_.IncreaseLevel();
   default_stmt.stmt->Accept(*this);
   indenter_.DecreaseLevel();
 }
 
 void AstDumper::Visit(const ExprStmtNode& expr_stmt) {
-  std::cout << indenter_.Indent() << "ExprStmtNode\n";
+  std::cout << indenter_.Indent() << "ExprStmtNode <" << expr_stmt.loc << ">\n";
   indenter_.IncreaseLevel();
   expr_stmt.expr->Accept(*this);
   indenter_.DecreaseLevel();
 }
 
 void AstDumper::Visit(const NullExprNode& null_expr) {
-  std::cout << indenter_.Indent() << "NullStmtNode\n";
+  std::cout << indenter_.Indent() << "NullStmtNode <" << null_expr.loc << ">\n";
 }
 
 void AstDumper::Visit(const IdExprNode& id_expr) {
-  std::cout << indenter_.Indent() << "IdExprNode " << id_expr.id << ": "
-            << TypeToString(id_expr.type) << '\n';
+  std::cout << indenter_.Indent() << "IdExprNode <" << id_expr.loc << "> "
+            << id_expr.id << ": " << TypeToString(id_expr.type) << '\n';
 }
 
 void AstDumper::Visit(const IntConstExprNode& int_expr) {
-  std::cout << indenter_.Indent() << "IntConstExprNode " << int_expr.val << ": "
-            << TypeToString(int_expr.type) << '\n';
+  std::cout << indenter_.Indent() << "IntConstExprNode <" << int_expr.loc
+            << "> " << int_expr.val << ": " << TypeToString(int_expr.type)
+            << '\n';
 }
 
 void AstDumper::Visit(const ArgExprNode& arg_expr) {
-  std::cout << indenter_.Indent() << "ArgExprNode "
+  std::cout << indenter_.Indent() << "ArgExprNode <" << arg_expr.loc << "> "
             << TypeToString(arg_expr.type) << '\n';
   indenter_.IncreaseLevel();
   arg_expr.arg->Accept(*this);
@@ -242,8 +251,8 @@ void AstDumper::Visit(const ArgExprNode& arg_expr) {
 }
 
 void AstDumper::Visit(const FuncCallExprNode& call_expr) {
-  std::cout << indenter_.Indent() << "FuncCallExprNode "
-            << TypeToString(call_expr.type) << '\n';
+  std::cout << indenter_.Indent() << "FuncCallExprNode <" << call_expr.loc
+            << "> " << TypeToString(call_expr.type) << '\n';
   indenter_.IncreaseLevel();
   call_expr.func_expr->Accept(*this);
   for (const auto& arg : call_expr.args) {
@@ -253,7 +262,7 @@ void AstDumper::Visit(const FuncCallExprNode& call_expr) {
 }
 
 void AstDumper::Visit(const UnaryExprNode& unary_expr) {
-  std::cout << indenter_.Indent() << "UnaryExprNode "
+  std::cout << indenter_.Indent() << "UnaryExprNode <" << unary_expr.loc << "> "
             << TypeToString(unary_expr.type) << " "
             << GetUnaryOperator(unary_expr.op) << '\n';
   indenter_.IncreaseLevel();
@@ -262,7 +271,7 @@ void AstDumper::Visit(const UnaryExprNode& unary_expr) {
 }
 
 void AstDumper::Visit(const BinaryExprNode& bin_expr) {
-  std::cout << indenter_.Indent() << "BinaryExprNode "
+  std::cout << indenter_.Indent() << "BinaryExprNode <" << bin_expr.loc << "> "
             << TypeToString(bin_expr.type) << " "
             << GetBinaryOperator(bin_expr.op) << '\n';
   indenter_.IncreaseLevel();
@@ -272,8 +281,9 @@ void AstDumper::Visit(const BinaryExprNode& bin_expr) {
 }
 
 void AstDumper::Visit(const SimpleAssignmentExprNode& assign_expr) {
-  std::cout << indenter_.Indent() << "SimpleAssignmentExprNode "
-            << TypeToString(assign_expr.type) << '\n';
+  std::cout << indenter_.Indent() << "SimpleAssignmentExprNode <"
+            << assign_expr.loc << "> " << TypeToString(assign_expr.type)
+            << '\n';
   indenter_.IncreaseLevel();
   assign_expr.lhs->Accept(*this);
   assign_expr.rhs->Accept(*this);
