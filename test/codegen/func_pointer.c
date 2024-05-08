d7a60617099aeec7138c987df32d473b984ebe2a
Lai-YT <381xvmvbib@gmail.com>
2024-05-08 16:07:24 +0800

    Support taking address of function

--- test/codegen/func_pointer.c
+++ test/codegen/func_pointer.c
@@ -7,5 +7,10 @@ int main() {
   p = add;
   int c = p(2, 3);
   __builtin_print(c);
+  // Taking address of a function has the same effect as using the function
+  // name.
+  p = &add;
+  int d = p(1, 2);
+  __builtin_print(d);
   return 0;
 }
