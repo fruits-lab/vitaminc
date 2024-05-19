a38fd417196f701b836da9296622d9ed0a664d67
Lee <lee10202013@gmail.com>
2024-05-19 18:11:03 +0800

    Implement logical operator code generation

--- test/codegen/bin_expr.c
+++ test/codegen/bin_expr.c
@@ -7,5 +7,9 @@ int main() {
   __builtin_print(-1 << 31);
   __builtin_print(65536 >> 16);
   __builtin_print(-1 >> 16);
+  __builtin_print(4 && 3);
+  __builtin_print(0 && 2);
+  __builtin_print(5 || 0);
+  __builtin_print(0 || 0);
   return 0;
 }
