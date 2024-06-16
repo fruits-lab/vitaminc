c312c58165d300c4b595a4810a0bf8d4b8faf837
Lee <lee10202013@gmail.com>
2024-06-16 20:51:21 +0800

    Write LLVM IR to files

--- src/llvm_ir_generator.cpp
+++ src/llvm_ir_generator.cpp
@@ -1,7 +1,10 @@
 #include "llvm_ir_generator.hpp"
 
+#include <llvm/IR/IRBuilder.h>
+
 #include "ast.hpp"
-#include "llvm/builder.hpp"
+
+using namespace llvm;
 
 void LLVMIRGenerator::Visit(const DeclStmtNode& decl_stmt) {}
 
@@ -17,13 +20,32 @@ void LLVMIRGenerator::Visit(const RecordVarDeclNode& struct_def) {}
 
 void LLVMIRGenerator::Visit(const ParamNode& parameter) {}
 
-void LLVMIRGenerator::Visit(const FuncDefNode& func_def) {}
+void LLVMIRGenerator::Visit(const FuncDefNode& func_def) {
+  auto i32 = builder_.getInt32Ty();
+  auto prototype = FunctionType::get(i32, false);
+  Function* fn = Function::Create(prototype, Function::ExternalLinkage,
+                                  func_def.id, module_.get());
+  BasicBlock* body = BasicBlock::Create(*context_, "body", fn);
+  builder_.SetInsertPoint(body);
+}
 
 void LLVMIRGenerator::Visit(const LoopInitNode& loop_init) {}
 
 void LLVMIRGenerator::Visit(const CompoundStmtNode& compound_stmt) {}
 
-void LLVMIRGenerator::Visit(const ProgramNode& program) {}
+void LLVMIRGenerator::Visit(const ProgramNode& program) {
+  // Generate builtin print function.
+  auto i32 = builder_.getInt32Ty();
+  auto prototype = FunctionType::get(i32, false);
+  Function* main_fn = Function::Create(prototype, Function::ExternalLinkage,
+                                       "__builtin_print__", module_.get());
+  BasicBlock* body = BasicBlock::Create(*context_, "body", main_fn);
+  builder_.SetInsertPoint(body);
+
+  for (const auto& func_def : program.func_def_list) {
+    func_def->Accept(*this);
+  }
+}
 
 void LLVMIRGenerator::Visit(const IfStmtNode& if_stmt) {}
 
