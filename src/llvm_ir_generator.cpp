bc0004717450ef6e29b40ac3884ea30f27c37f56
Lee <lee10202013@gmail.com>
2024-06-22 16:02:26 +0800

    Handle simple binary expression

--- src/llvm_ir_generator.cpp
+++ src/llvm_ir_generator.cpp
@@ -8,6 +8,30 @@
 
 namespace {
 
+llvm::Instruction::BinaryOps GetBinaryOperator(BinaryOperator op) {
+  switch (op) {
+    case BinaryOperator::kAdd:
+      return llvm::BinaryOperator::Add;
+    case BinaryOperator::kSub:
+      return llvm::BinaryOperator::Sub;
+    case BinaryOperator::kMul:
+      return llvm::BinaryOperator::Mul;
+    case BinaryOperator::kDiv:
+      return llvm::BinaryOperator::SDiv;
+    case BinaryOperator::kMod:
+      return llvm::BinaryOperator::SRem;
+    case BinaryOperator::kAnd:
+      return llvm::BinaryOperator::And;
+    case BinaryOperator::kXor:
+      return llvm::BinaryOperator::Xor;
+    case BinaryOperator::kOr:
+      return llvm::BinaryOperator::Or;
+    default:
+      // TODO
+      return llvm::BinaryOperator::Xor;
+  }
+}
+
 /// @brief Store LLVM Value class at the bottom level of AST node. Upper level
 /// AST node can use the information in Value directly.
 class PrevValueRecorder {
@@ -31,8 +55,6 @@ auto
     = PrevValueRecorder{};
 }  // namespace
 
-using namespace llvm;
-
 void LLVMIRGenerator::Visit(const DeclStmtNode& decl_stmt) {
   for (const auto& decl : decl_stmt.decls) {
     decl->Accept(*this);
@@ -53,12 +75,13 @@ void LLVMIRGenerator::Visit(const ParamNode& parameter) {}
 
 void LLVMIRGenerator::Visit(const FuncDefNode& func_def) {
   auto i32 = builder_.getInt32Ty();
-  auto prototype = FunctionType::get(i32, false);
-  auto* fn = Function::Create(prototype,
-                              func_def.id == "main" ? Function::ExternalLinkage
-                                                    : Function::InternalLinkage,
-                              func_def.id, module_.get());
-  auto* body = BasicBlock::Create(*context_, "body", fn);
+  auto prototype = llvm::FunctionType::get(i32, false);
+  auto* fn = llvm::Function::Create(prototype,
+                                    func_def.id == "main"
+                                        ? llvm::Function::ExternalLinkage
+                                        : llvm::Function::InternalLinkage,
+                                    func_def.id, module_.get());
+  auto* body = llvm::BasicBlock::Create(*context_, "body", fn);
   builder_.SetInsertPoint(body);
   func_def.body->Accept(*this);
 }
@@ -78,9 +101,9 @@ void LLVMIRGenerator::Visit(const ProgramNode& program) {
   auto ptr = builder_.getPtrTy();
   std::vector<llvm::Type*> params;
   params.push_back(ptr);
-  auto prototype = FunctionType::get(i32, params, true);
-  Function::Create(prototype, Function::ExternalLinkage, "printf",
-                   module_.get());
+  auto prototype = llvm::FunctionType::get(i32, params, true);
+  llvm::Function::Create(prototype, llvm::Function::ExternalLinkage, "printf",
+                         module_.get());
 
   for (const auto& func_def : program.func_def_list) {
     func_def->Accept(*this);
@@ -115,7 +138,9 @@ void LLVMIRGenerator::Visit(const CaseStmtNode& case_stmt) {}
 
 void LLVMIRGenerator::Visit(const DefaultStmtNode& default_stmt) {}
 
-void LLVMIRGenerator::Visit(const ExprStmtNode& expr_stmt) {}
+void LLVMIRGenerator::Visit(const ExprStmtNode& expr_stmt) {
+  expr_stmt.expr->Accept(*this);
+}
 
 void LLVMIRGenerator::Visit(const InitExprNode& init_expr) {}
 
@@ -130,23 +155,55 @@ void LLVMIRGenerator::Visit(const IdExprNode& id_expr) {}
 void LLVMIRGenerator::Visit(const IntConstExprNode& int_expr) {
   auto i32 = builder_.getInt32Ty();
   auto* addr = builder_.CreateAlloca(i32);
-  auto* val = ConstantInt::get(i32, int_expr.val, true);
+  auto* val = llvm::ConstantInt::get(i32, int_expr.val, true);
   builder_.CreateStore(val, addr);
   val_recorder.Record(addr);
 }
 
-void LLVMIRGenerator::Visit(const ArgExprNode& arg_expr) {}
+void LLVMIRGenerator::Visit(const ArgExprNode& arg_expr) {
+  arg_expr.arg->Accept(*this);
+}
 
 void LLVMIRGenerator::Visit(const ArrSubExprNode& arr_sub_expr) {}
 
 void LLVMIRGenerator::Visit(const CondExprNode& cond_expr) {}
 
-void LLVMIRGenerator::Visit(const FuncCallExprNode& call_expr) {}
+void LLVMIRGenerator::Visit(const FuncCallExprNode& call_expr) {
+  call_expr.func_expr->Accept(*this);
+
+  // Evaluate the arguments.
+  std::vector<llvm::Value*> arg_vals{};
+  for (const auto& arg : call_expr.args) {
+    arg->Accept(*this);
+    auto* arg_val = val_recorder.ValOfPrevExpr();
+    arg_vals.push_back(arg_val);
+  }
+}
 
 void LLVMIRGenerator::Visit(const PostfixArithExprNode& postfix_expr) {}
 
 void LLVMIRGenerator::Visit(const UnaryExprNode& unary_expr) {}
 
-void LLVMIRGenerator::Visit(const BinaryExprNode& bin_expr) {}
+void LLVMIRGenerator::Visit(const BinaryExprNode& bin_expr) {
+  bin_expr.lhs->Accept(*this);
+  auto* lhs = val_recorder.ValOfPrevExpr();
+
+  if (bin_expr.op == BinaryOperator::kComma) {
+    // For the comma operator, the value of its left operand is not used and can
+    // be eliminated if it has no side effects or if its definition is
+    // immediately dead. However, we leave these optimizations to QBE.
+    bin_expr.rhs->Accept(*this);
+    auto* rhs = val_recorder.ValOfPrevExpr();
+    val_recorder.Record(rhs);
+    return;
+  }
+  if (bin_expr.op == BinaryOperator::kLand ||
+      bin_expr.op == BinaryOperator::kLor) {
+    // TODO
+  } else {
+    auto* rhs = val_recorder.ValOfPrevExpr();
+    builder_.CreateBinOp(GetBinaryOperator(bin_expr.op), lhs, rhs);
+  }
+}
 
 void LLVMIRGenerator::Visit(const SimpleAssignmentExprNode& assign_expr) {}
