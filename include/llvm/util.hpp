b4a9c168007ac088ea610bede5430d1814950427
Lee <lee10202013@gmail.com>
2024-06-28 23:00:46 +0800

    Implement Bitwise complement operator code generation

--- include/llvm/util.hpp
+++ include/llvm/util.hpp
@@ -11,17 +11,6 @@ class Util {
   /// @brief Integer type
   llvm::IntegerType* i32Ty;
 
-  /// @brief NOT operation wrapper since LLVM doesn't provide NOT operator.
-  /// @return
-  llvm::Value* NotOperation(llvm::Value* val) {
-    // Is 0 if the value of its operand compares unequal to 0, 1 if the value
-    // of its operand compares equal to 0.
-    auto zero = llvm::ConstantInt::get(val->getType(), 0, true);
-    auto one = llvm::ConstantInt::get(val->getType(), 1, true);
-    auto* isZero = builder_->CreateICmpEQ(val, zero, "isZero");
-    return builder_->CreateSelect(isZero, one, zero, "notResult");
-  };
-
   Util(std::unique_ptr<llvm::IRBuilder<>>& builder) : builder_{builder} {
     i32Ty = builder_->getInt32Ty();
   }
