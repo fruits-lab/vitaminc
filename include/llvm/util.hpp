99b707329ac2deefc1604056ed7a573c9a207528
Lee <lee10202013@gmail.com>
2024-07-01 01:56:34 +0800

    Implement switch, case, default statement code generation

--- include/llvm/util.hpp
+++ include/llvm/util.hpp
@@ -34,6 +34,26 @@ class Util {
     }
   }
 
+  /// @brief Create a branch instruction to the next basic block.
+  void CurrBBFallThroughNextBB(llvm::BasicBlock* curr_BB,
+                               llvm::BasicBlock* next_BB) {
+    auto BB = curr_BB;
+    bool has_terminator = false;
+    for (auto it = BB->begin(); it != BB->end();) {
+      if (it->isTerminator()) {
+        has_terminator = true;
+        break;
+      } else {
+        ++it;
+      }
+    }
+
+    if (!has_terminator) {
+      builder_->SetInsertPoint(curr_BB);
+      builder_->CreateBr(next_BB);
+    }
+  }
+
   Util(std::unique_ptr<llvm::IRBuilder<>>& builder) : builder_{builder} {
     intTy = builder_->getInt32Ty();
     intPtrTy = builder_->getPtrTy();
