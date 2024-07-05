69d6cee126cb8b88047d2e1a4b364f161f549670
Lee <lee10202013@gmail.com>
2024-07-05 14:48:50 +0800

    Refactor helper function for basic blocks

--- src/llvm/util.cpp
+++ src/llvm/util.cpp
@@ -16,37 +16,25 @@
 
 using namespace util;
 
-void LLVMIRBuilderHelper::CreateBrIfNoBrBefore(llvm::BasicBlock* next_bb) {
-  auto bb = builder_.GetInsertBlock();
-  bool has_terminator = false;
-  for (auto it = bb->begin(); it != bb->end();) {
+bool LLVMIRBuilderHelper::HasTerminator(llvm::BasicBlock* bb) {
+  for (auto it = bb->begin(); it != bb->end(); ++it) {
     if (it->isTerminator()) {
-      has_terminator = true;
-      break;
-    } else {
-      ++it;
+      return true;
     }
   }
+  return false;
+}
 
-  if (!has_terminator) {
+void LLVMIRBuilderHelper::CreateBrIfNoBrBefore(llvm::BasicBlock* next_bb) {
+  auto bb = builder_.GetInsertBlock();
+  if (!HasTerminator(bb)) {
     builder_.CreateBr(next_bb);
   }
 }
 
 void LLVMIRBuilderHelper::CurrBBFallThroughNextBB(llvm::BasicBlock* curr_bb,
                                                   llvm::BasicBlock* next_bb) {
-  auto bb = curr_bb;
-  bool has_terminator = false;
-  for (auto it = bb->begin(); it != bb->end();) {
-    if (it->isTerminator()) {
-      has_terminator = true;
-      break;
-    } else {
-      ++it;
-    }
-  }
-
-  if (!has_terminator) {
+  if (!HasTerminator(curr_bb)) {
     builder_.SetInsertPoint(curr_bb);
     builder_.CreateBr(next_bb);
   }
@@ -59,7 +47,6 @@ llvm::BasicBlock* LLVMIRBuilderHelper::FindBBWithNameOf(const std::string& id) {
       return &(bb_iter);
     }
   }
-
   return nullptr;
 }
 
@@ -72,7 +59,6 @@ llvm::Type* LLVMIRBuilderHelper::GetLLVMType(const Type& type) {
     if (type.IsEqual(PrimitiveType::kInt)) {
       return builder_.getInt32Ty();
     }
-
     throw std::runtime_error{"unknown type in GetLLVMType!"};
   } else if (type.IsPtr()) {
     auto ptr_type = dynamic_cast<const PtrType*>(&type);
@@ -88,14 +74,13 @@ llvm::Type* LLVMIRBuilderHelper::GetLLVMType(const Type& type) {
     return llvm::ArrayType::get(GetLLVMType(arr_type->element_type()),
                                 arr_type->len());
   } else if (type.IsStruct() || type.IsUnion()) {
-    // A prefix is needed to distinguish struct and union with the same name.
-    std::string record_prefix = type.IsStruct() ? "struct_" : "union_";
     auto record_type = dynamic_cast<const RecordType*>(&type);
     std::vector<llvm::Type*> field_types;
     for (auto& field : record_type->fields()) {
       field_types.push_back(GetLLVMType(*(field->type)));
     }
-
+    // A prefix is needed to distinguish struct and union with the same name.
+    std::string record_prefix = type.IsStruct() ? "struct_" : "union_";
     return llvm::StructType::create(builder_.getContext(), field_types,
                                     record_prefix + record_type->id());
   } else if (type.IsFunc()) {
@@ -105,7 +90,6 @@ llvm::Type* LLVMIRBuilderHelper::GetLLVMType(const Type& type) {
     for (auto& param_type : func_type->param_types()) {
       param_types.push_back(GetLLVMType(*param_type));
     }
-
     return llvm::FunctionType::get(return_type, param_types, false);
   }
 
