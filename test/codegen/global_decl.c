34cab81bf4fd8a395d9eedf33128feb835cd9128
Lee <lee10202013@gmail.com>
2024-07-06 22:41:32 +0800

    Update global variable assignment expression and test case

--- test/codegen/global_decl.c
+++ test/codegen/global_decl.c
@@ -6,9 +6,13 @@ int a[3] = {6, 5, 3};
 
 int main() {
   __builtin_print(c);
+  d = 4;
   __builtin_print(d);
 
   __builtin_print(b[0]);
+
+  a[0] = 7;
+  a[2] = 4;
   __builtin_print(a[0]);
   __builtin_print(a[1]);
   __builtin_print(a[2]);
