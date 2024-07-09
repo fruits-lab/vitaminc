74b254557417eadb99b9af54baf677f8578e5d35
Lai-YT <381xvmvbib@gmail.com>
2024-07-09 12:32:35 +0800

    Upgrade clang format version to 18 in CI

--- scripts/check-format.sh
+++ scripts/check-format.sh
@@ -5,14 +5,15 @@
 # otherwise, exits with the count of violations.
 #
 
+CLANG_FORMAT=${CLANG_FORMAT:-clang-format}
 SOURCES=$(find "$(git rev-parse --show-toplevel)" | grep -E "\.(c|h)pp\$")
 
 total_violation_count=0
 for file in $SOURCES; do
   # reports violations
-  clang-format --dry-run "$file"
+  $CLANG_FORMAT --dry-run "$file"
   # count violations
-  violation_count=$(clang-format --output-replacements-xml "$file" | grep -E -c "</replacement>")
+  violation_count=$($CLANG_FORMAT --output-replacements-xml "$file" | grep -E -c "</replacement>")
   total_violation_count=$((total_violation_count + violation_count))
 done
 exit "$total_violation_count"
